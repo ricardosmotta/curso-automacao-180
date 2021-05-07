c3f740bbcf5dd433c7d1ab13f2be02fa3da68be3
Ricardo Motta <rmotta.qa@gmail.com>
2021-05-07 09:09:24 -0300

    avanço no curso
    bibliotecas cucumber, capybara, selenium

---
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -0,0 +1,11 @@
+Dado('que acesso a página de cadastro') do
+    visit "http://rocklov-web:3000/signup"
+end
+
+Quando('submeto meus dados') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('sou redirecionado para o dashboard') do
+    pending # Write code here that turns the phrase above into concrete actions
+end
