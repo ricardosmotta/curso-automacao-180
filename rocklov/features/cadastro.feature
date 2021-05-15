a782ee083312db0fdd5f14e3acf8ac72b1d9b879
Ricardo Motta <rmotta.qa@gmail.com>
2021-05-15 17:42:56 -0300

    Cadastro
    Automatização da tela de cadastro e seus cenários

--- rocklov/features/cadastro.feature
+++ rocklov/features/cadastro.feature
@@ -6,38 +6,43 @@ Funcionalidade: Cadastro
     Quero fazer o meu cadastro no RockLov
     Para que eu possa disponibilizá-los para locação
 
-    @temp
+    @macgyver
     Cenario: Efetuar cadastro
 
         Dado que acesso a página de cadastro
         Quando submeto meus dados
         Então sou redirecionado para o dashboard
-        
+
+    @tentativa_cadastro
     Cenario: Submeter cadastro SEM nome
 
         Dado que acesso a página de cadastro
         Quando submeto meus dados SEM o nome
         Então vejo a mensagem de alerta: Oops. Informe seu nome completo!
 
+    @tentativa_cadastro
     Cenario: Submeter cadastro SEM o e-mail
 
         Dado que acesso a página de cadastro
         Quando submeto meus dados SEM o e-mail
         Então vejo a mensagem de alerta: Oops. Informe um e-mail válido!
 
-    Cenario: Submeter cadastro com e-mail incorreto
+     @tentativa_cadastro
+    Cenario: Submeter cadastro com e-mail inválido
 
         Dado que acesso a página de cadastro
         Quando submeto meus dados com e-mail incorreto
         Então vejo a mensagem de alerta: Oops. Informe um e-mail válido!
 
+    @tentativa_cadastro
     Cenario: Submeter cadastro SEM a senha
 
         Dado que acesso a página de cadastro
         Quando submeto meus dados SEM a senha
         Então vejo a mensagem de alerta: Oops. Informe sua senha secreta!
 
-    Cenario: Email já cadastrado
+    @tentativa_cadastro
+    Cenario: Email ja cadastrado
 
         Dado que acesso a página de cadastro
         Quando submeto os dados com um e-mail já cadastrado
--- rocklov/features/step_definitions/cadastro_steps.rb
+++ rocklov/features/step_definitions/cadastro_steps.rb
@@ -4,10 +4,8 @@ end
 
 Quando('submeto meus dados') do
     find("#fullName").set "Ricardo Motta"
-    find("#email").set "teste@google.com"
+    find("#email").set Faker::Internet.free_email
     find("#password").set "teste"
-
-    sleep 5 # temporário 
 
     click_button "Cadastrar"
 end
@@ -15,3 +13,60 @@ end
 Então('sou redirecionado para o dashboard') do
     expect(page).to have_css ".dashboard"
 end
+
+Quando('submeto meus dados SEM o nome') do
+    find("#email").set Faker::Internet.free_email
+    find("#password").set "teste"
+
+    click_button "Cadastrar"
+end
+
+Então('vejo a mensagem de alerta: Oops. Informe seu nome completo!') do
+    alert = find(".alert")
+    expect(alert.text).to eql "Oops. Informe seu nome completo!"
+end
+
+Quando('submeto meus dados SEM o e-mail') do
+    find("#fullName").set "Ricardo Motta"
+    find("#password").set "teste"
+
+    click_button "Cadastrar"
+  end
+
+  Quando('submeto meus dados com e-mail incorreto') do
+    find("#fullName").set "Ricardo Motta"
+    find("#email").set "ping*pong.com"
+    find("#password").set "teste"
+
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe um e-mail válido!') do
+    alert = find(".alert")
+    expect(alert.text).to eql "Oops. Informe um email válido!"
+  end
+
+  Quando('submeto meus dados SEM a senha') do
+    find("#fullName").set "Ricardo Motta"
+    find("#email").set Faker::Internet.free_email
+
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Oops. Informe sua senha secreta!') do
+    alert = find(".alert")
+    expect(alert.text).to eql "Oops. Informe sua senha secreta!"
+  end
+
+  Quando('submeto os dados com um e-mail já cadastrado') do
+    find("#fullName").set "Ricardo Motta"
+    find("#email").set "ping@pong.com"
+    find("#password").set "teste"
+    sleep 10 #temporario
+    click_button "Cadastrar"
+  end
+  
+  Então('vejo a mensagem de alerta: Email já cadastrado') do
+    alert_error = find(".alert-error")
+    expect(alert_error.text).to eql "🤭 Email já cadastrado."
+  end
