a55a9616851231682488b587e5618dff963e5dc9
Ricardo Motta <rmotta.qa@gmail.com>
2021-06-07 22:03:52 -0300

    Update - heranças

--- code4testers/veiculo.rb
+++ code4testers/veiculo.rb
@@ -4,7 +4,7 @@
 
 # Uma classe é um objeto que tem características e funções
 
-class Carro
+class Veiculo
     attr_accessor :nome, :cor, :marca   # attr_acessor atribuí características à uma classe
 
     def initialize(nome, cor, marca)
@@ -13,8 +13,29 @@ class Carro
         @marca = marca
     end
 
+    def define_cor(cor)
+        @cor = cor
+    end
+
+end
+
+class Carro < Veiculo
     def ligar  
         puts "O #{@nome} cor #{@cor} da #{@marca} está pronto para iniciar o trajeto."
+    end
+
+    def dirigir
+        puts "O #{@nome} está iniciando o trajeto."
+    end
+end
+
+class Moto < Veiculo
+    def ligar  
+        puts "A #{@nome} cor #{@cor} da #{@marca} está pronta para iniciar o trajeto."
+    end
+
+    def pilotar
+        puts "A #{@nome} está iniciando o trajeto."
     end
 end
 
@@ -23,9 +44,9 @@ chevette = Carro.new("Chevette", "vermelho", "GM")
 # chevette.nome = "Chevette"
 # chevette.cor = "vermelho"
 # chevette.marca = "GM"
-
 chevette.ligar
-
+chevette.dirigir
+chevette.define_cor("laranja")
 fusca = Carro.new("Fusca", "preto", "VW")
 
 # fusca.nome = "Fusca"
@@ -33,3 +54,10 @@ fusca = Carro.new("Fusca", "preto", "VW")
 # fusca.marca = "VW"
 
 fusca.ligar
+fusca.dirigir
+fusca.define_cor("branca")
+
+cg = Moto.new("CG", "azul", "Honda")
+cg.ligar
+cg.pilotar
+cg.define_cor("verde")
