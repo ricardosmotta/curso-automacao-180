78864f2bee0ed7cced54508899840a205fa45d25
Ricardo Motta <rmotta.qa@gmail.com>
2021-06-07 21:38:03 -0300

    Update - método initialize
    Aula "Construtores"

--- code4testers/carro.rb
+++ code4testers/carro.rb
@@ -7,24 +7,29 @@
 class Carro
     attr_accessor :nome, :cor, :marca   # attr_acessor atribuí características à uma classe
 
+    def initialize(nome, cor, marca)
+        @nome = nome
+        @cor = cor
+        @marca = marca
+    end
+
     def ligar  
-        puts "O #{@nome} está pronto para iniciar o trajeto."
+        puts "O #{@nome} cor #{@cor} da #{@marca} está pronto para iniciar o trajeto."
     end
 end
 
-chevette = Carro.new
+chevette = Carro.new("Chevette", "vermelho", "GM")
 
-chevette.nome = "Chevette"
-chevette.cor = "vermelho"
-chevette.marca = "GM"
+# chevette.nome = "Chevette"
+# chevette.cor = "vermelho"
+# chevette.marca = "GM"
 
 chevette.ligar
-puts chevette.class
 
-fusca = Carro.new
+fusca = Carro.new("Fusca", "preto", "VW")
 
-fusca.nome = "Fusca"
-fusca.cor = "preto"
-fusca.marca = "VW"
+# fusca.nome = "Fusca"
+# fusca.cor = "preto"
+# fusca.marca = "VW"
 
 fusca.ligar
